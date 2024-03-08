6dbafb73d251e9e6ac53f57120f1e8c4b8487acc
[satuhalinen] <satuh9@gmail.com>
2024-03-08 15:02:52 +0200

    add the third lesson

--- src/app/countries/CountriesAPI.tsx
+++ src/app/countries/CountriesAPI.tsx
@@ -2,9 +2,11 @@
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
+import {Card} from "react-bootstrap"
 
-interface CountryCapital {
+interface CountryDetails {
   capital: string[];
+  name: CountryInfo;
 }
 
 interface CountryInfo {
@@ -23,7 +25,7 @@ interface LanguageDetails {
 }
 
 const CountriesAPI = () => {
-  const [countries, setCountries] = useState<CountryCapital[]>([]);
+  const [countries, setCountries] = useState<CountryDetails[]>([]);
   const getCountriesInfo = async () => {
     const response = await axios.get(
        "https://restcountries.com/v3.1/all?fields=name,capital"
@@ -40,9 +42,25 @@ const CountriesAPI = () => {
   <>
   <div>Countries API will be here</div>
   {countries.map((country, index) => (
+    <Card 
+    key={`${country.capital}_${index}`}
+    style={{maxWidth: "20rem", height: "10rem"}}
+    >
 <div key={`${country.capital}_${index}`}>
   <h2>{country.capital[0]}</h2>
+  <p>Country Name: {country.name.official[0]}</p>
+  <p>{}</p>
+  {Object.entries(country.name.nativeName).map(([key, value]) => (
+    <div key={key}>
+    <p key={key}>
+      {key}: {value.common}
+    </p>
+    <p>{value.common}</p>
+    <p>{value.official}</p>
+    </div>
+  ))}
 </div>
+</Card>
   ))}
   </>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
