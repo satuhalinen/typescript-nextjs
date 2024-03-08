9c04cc0c00b1f412a3a6ba49fcd4259cc9d07c01
[satuhalinen] <satuh9@gmail.com>
2024-03-08 12:08:27 +0200

    add first lesson

---
+++ src/app/countries/CountriesDetail.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+interface IProps {
+    countries: Country[];
+}
+    interface Country {
+        name: string;
+        capital: string;
+        population: number;
+        area: number;
+        flag: string;
+    }
+
+    const CountriesDetail = (props: IProps) => {
+    return (
+        <div>
+            <h1>Country Detail</h1>
+            {props.countries.map((country) => (
+                <div key={country.name}>
+                    <h2>{country.name}</h2>
+                    <p>Capital: {country.capital}</p>
+                    <p>Population: {country.population}</p>
+                    <p>Area: {country.area}</p>
+                    <Image src={country.flag} alt={country.name} width="300" height="400"/>
+                    </div>
+            ))}
+        </div>
+    )
+}
+
+export default CountriesDetail;
